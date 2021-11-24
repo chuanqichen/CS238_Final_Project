7f2a72c26b3f5108adf70b1e37eee8d6eafa0268
Yutai Zhou <yutai.zhou@ll.mit.edu>
2021-11-23 22:55:54 -0500

    refactored board.jl to use CommonRLInterface.jl instead of our homebrew MDP struct

--- 2048/board.jl
+++ 2048/board.jl
@@ -1,17 +1,41 @@
+using Parameters
+using CommonRLInterface;
+const rli = CommonRLInterface;
 
-using Game2048: Dirs, bitboard_to_array, move
+using Game2048: Dirs, bitboard_to_array, move, Bitboard
 
-function possible_moves(board)
-    moves = []
+@with_kw mutable struct Env2048 <: AbstractEnv
+    goal::Int; @assert ispow2(goal)
+    γ::Float64
+    board::Bitboard
+end
+
+function Env2048(goal::Int=2048, γ::Float64=1.0)
+    Env2048(goal=goal, γ=γ, board=initbboard())
+end
+
+function rli.reset!(env::Env2048)
+    env.board = initbboard()
+end
+rli.actions(env::Env2048) = instances(Dirs)
+rli.observe(env::Env2048) = env.board
+rli.terminated(env::Env2048) = (env.goal == (2 ^ maximum(bitboard_to_array(env.board))))
+function rli.act!(env::Env2048, a) 
+    new_board = move(env.board, a)
+    env.board = new_board
+end
+
+@provide rli.valid_actions(env::Env2048) = keys(valid_transitions(env.board))
+
+function valid_transitions(board::Bitboard)::Dict{Dirs, Bitboard}
     boards = Dict()
-    for dir in instances(Dirs)
-        temp_board = move(board, dir)
+    for direction in instances(Dirs)
+        temp_board = move(board, direction)
         if temp_board != board
-            push!(moves, dir)
-            boards[dir] = temp_board
+            boards[direction] = temp_board
         end
     end
-    return (moves, boards)
+    return boards
 end
 
 function get_linear_value(board)
