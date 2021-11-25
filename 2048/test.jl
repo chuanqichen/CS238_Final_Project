51dacf1eaac8c6a39bac3178f8dec18a40fb653b
Yutai Zhou <yutai.zhou@ll.mit.edu>
2021-11-25 10:01:16 -0500

    added RL loop into test.jl

--- 2048/test.jl
+++ 2048/test.jl
@@ -15,10 +15,10 @@ env = Env2048(
     bb_repr = false
     )
 println(actions(env))
-println([Int(a) for a in actions(env)])
 
 # Algorithm Spec
-U(s) = simulate_game(s)
+# U(s) = simulate_game(s)
+U(s, goal, curr_step, max_step) = rollout(s, rli.actions(env), goal, curr_step, max_step)
 # U_1(s) = simulate_weighted(s)
 d = 2
 exploration_factor = 0.9
@@ -37,36 +37,35 @@ mcts = MonteCarloTreeSearch(
     U = U, 
 )
 
-function play()
-    init_board = initbboard()
-    possible = keys(valid_transitions(init_board))
-
-    curr_board = init_board
-    while (length(possible)>0)
-        action_to_take = mcts(curr_board)
-        curr_board = move(curr_board, Dirs(action_to_take))
-        curr_board = add_tile(curr_board)
-        possible = keys(valid_transitions(curr_board))
-        display(curr_board); println()
-    end
-    display(curr_board); println()
-    println("Score: $(get_value(curr_board))")
-    println("Maximum Tile: $(maximum_tile_value(curr_board))")
-end
-
 # function play()
-#     reset!(env)
-#     valid_actions = rli.valid_actions(env)
-#     curr_board = rli.state(env)
-#     while (length(valid_actions)>0)
+#     init_board = initbboard()
+#     possible = keys(valid_transitions(init_board))
+
+#     curr_board = init_board
+#     while (length(possible)>0)
 #         action_to_take = mcts(curr_board)
-#         rli.act!(env, action_to_take)
-#         valid_actions = rli.valid_actions(env)
-#         display(env.board); println()
+#         curr_board = move(curr_board, Dirs(action_to_take))
+#         curr_board = add_tile(curr_board)
+#         possible = keys(valid_transitions(curr_board))
+#         display(curr_board); println()
 #     end
-#     # display(env.board); println()
-#     println("Score: $(get_value(env.board))")
-#     println("Maximum Tile: $(maximum_tile_value(env.board))")
+#     display(curr_board); println()
+#     println("Score: $(get_value(curr_board))")
+#     println("Maximum Tile: $(maximum_tile_value(curr_board))")
 # end
+
+function play()
+    reset!(env)
+    while !rli.terminated(env)
+        curr_board = rli.state(env)
+        valid_actions = rli.valid_actions(env)
+        action_to_take = mcts(curr_board)
+        println("\n $valid_actions -> $action_to_take")
+        display(env.board); 
+        rli.act!(env, action_to_take)
+    end
+    println("\nScore: $(get_value(env.board))")
+    println("Maximum Tile: $(maximum_tile_value(env.board))")
+end
 
 play()
